2d9d27564939c88c3b4931092d102e206e9258c2
Illo Yoon <yildbs@gmail.com>
2017-08-08 23:08:43 +0900

    * First commit

---
+++ src/ylpr/Recognizer.cpp
@@ -0,0 +1,21 @@
+/*
+ * Recognizer.cpp
+ *
+ *  Created on: Aug 8, 2017
+ *      Author: yildbs
+ */
+
+#include "Recognizer.h"
+
+namespace ylpr{
+
+Recognizer::Recognizer() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Recognizer::~Recognizer() {
+	// TODO Auto-generated destructor stub
+}
+
+}
---
+++ src/ylpr/Recognizer.h
@@ -0,0 +1,21 @@
+/*
+ * Recognizer.h
+ *
+ *  Created on: Aug 8, 2017
+ *      Author: yildbs
+ */
+
+#ifndef RECOGNIZER_H_
+#define RECOGNIZER_H_
+
+namespace ylpr{
+
+class Recognizer {
+public:
+	Recognizer();
+	virtual ~Recognizer();
+};
+
+}
+
+#endif /* RECOGNIZER_H_ */
